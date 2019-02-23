f20930bb058b4e09989c17bfed390b35b1367d09
Kalin Kiesling <krkiesling@gmail.com>
2019-02-23 12:05:01 -0600

    regenerated ww mesh

---
+++ toyproblem/wwfiles/gen_wwig.py
@@ -0,0 +1,8 @@
+import vol as v
+
+g = v.IsoVolume()
+g.generate_levels(7, 1.e-5, 0.1, log=True)
+g.generate_volumes('./ww_mesh.vtk', 'ww_n')
+g.create_geometry(tag_groups=True, tag_for_viz=True)
+g.write_geometry(sname='wwig.h5m')
+g.write_geometry(sname='wwig.vtk')
