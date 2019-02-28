019b224b717eaba55e5d8922465a03bceddc2266
Kalin Kiesling <krkiesling@gmail.com>
2019-02-28 12:19:33 -0600

    added normalization factor from CADIS

--- toyproblem/wwfiles/gen_wwig.py
+++ toyproblem/wwfiles/gen_wwig.py
@@ -3,6 +3,6 @@ import vol as v
 g = v.IsoVolume()
 g.generate_levels(7, 1.e-5, 0.1, log=True)
 g.generate_volumes('./ww_mesh.vtk', 'ww_n')
-g.create_geometry(tag_groups=True, tag_for_viz=True)
+g.create_geometry(tag_groups=True, tag_for_viz=True, norm=1.990133553e3)
 g.write_geometry(sname='wwig.h5m')
 g.write_geometry(sname='wwig.vtk')
