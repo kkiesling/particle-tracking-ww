db6a509f2a6ab49bbc17a8839a9e6a56241006f1
Kalin Kiesling <krkiesling@gmail.com>
2019-03-02 14:48:40 -0600

    ww file cleanup

--- toyproblem/wwfiles/gen_wwig-fw.py
+++
@@ -1,8 +0,0 @@
-import vol as v
-
-g = v.IsoVolume()
-g.generate_levels(6, 0.01, 0.18, log=False)
-g.generate_volumes('./wwmesh-point.vtk', 'ww_n')
-g.create_geometry(tag_groups=True, tag_for_viz=True, norm=2.078987641)
-g.write_geometry(sname='wwig-point.h5m')
-g.write_geometry(sname='wwig-point.vtk')
--- toyproblem/wwfiles/gen_wwig.py
+++ toyproblem/wwfiles/gen_wwig.py
@@ -1,8 +1,8 @@
 import vol as v
 
 g = v.IsoVolume()
-g.generate_levels(7, 1.e-5, 0.1, log=True)
-g.generate_volumes('./ww_mesh.vtk', 'ww_n')
-g.create_geometry(tag_groups=True, tag_for_viz=True, norm=1.990133553e3)
+g.generate_levels(4, 8.e-7, 1.7e-6, log=False)
+g.generate_volumes('./mesh_ww_tags.vtk', 'ww_n')
+g.create_geometry(tag_groups=True, tag_for_viz=True, norm=1.890744809e+05)
 g.write_geometry(sname='wwig.h5m')
 g.write_geometry(sname='wwig.vtk')
