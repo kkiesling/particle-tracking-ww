ace468a0eacdffc7b248fdcc62586617392510f4
Kalin Kiesling <krkiesling@gmail.com>
2019-10-30 13:47:01 -0500

    removed old stuff

--- toyproblem/wwfiles/gen_wwig.py
+++
@@ -1,8 +0,0 @@
-import vol as v
-
-g = v.IsoVolume()
-g.generate_levels(4, 8.e-7, 1.7e-6, log=False)
-g.generate_volumes('./mesh_ww_tags.vtk', 'ww_n')
-g.create_geometry(tag_groups=True, tag_for_viz=True, norm=1.890744809e+05)
-g.write_geometry(sname='wwig.h5m')
-g.write_geometry(sname='wwig.vtk')
