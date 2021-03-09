58eaa0554f684e28d5b6fe939fa54c0d5d4cc795
Kalin Kiesling <krkiesling@gmail.com>
2021-03-09 16:01:26 -0600

    script for generating wwmesh with multiple grouos

--- wwig-geoms/multi-group/create-wwmesh.py
+++ wwig-geoms/multi-group/create-wwmesh.py
@@ -1,18 +1,44 @@
-# from pyne.mesh import Mesh, NativeMeshTag
+from pyne.mesh import Mesh, NativeMeshTag
 import numpy as np
 
 # mesh divisions
-cx = np.linspace(0., 20., 21, endpoints=True) #[0.0, 2.0, 4.0, 6.0, 8.0, 10.0]
-print(cx)
-cy = [-2.5, 2.5]
-cz = [-2.5, 2.5]
+cx = np.linspace(0., 20., 21, endpoint=True)
+cy = [-5., 5.]
+cz = [-5., 5.]
+num_mesh = (len(cx) - 1) * (len(cy) - 1) * (len(cz) - 1)
 
-# create mesh
-# m = Mesh(structured=True, structured_coords=[cx, cy, cz])
-#
-# # tag with values
-# m.tag('ww_n', tagtype=NativeMeshTag, size=1, dtype=float)
-# vals = [0.5, 0.1, 0.02, 0.004, 0.0008]
-# m.ww_n[:] = vals
-#
-# m.write_hdf5('ww-mesh.h5m', write_mats=False)
+# ww values for each mesh voxel region
+ww_vals = [0.1, 0.02, 0.004, 0.0008, 0.00016]
+#w_vals_4 = [0.1, 0.02, 0.004, 0.0008] # highest group has one less mesh spacing
+
+# ww surface values:
+# ww_surfs = [0.06, 0.012, 0.0024, 0.00048]
+
+# x coordinates where each surface will be located, key is energy group id
+w_loc = {0: [0, 1, 6, 11, 16, 20],  # E0
+         1: [0, 2, 7, 12, 17, 20],  # E1
+         2: [0, 3, 8, 13, 18, 20],  # E2
+         3: [0, 4, 9, 14, 19, 20],  # E3
+         4: [0, 5, 10, 15, 20]}     # E4
+num_egrp = len(w_loc)
+
+# create mesh for data population
+m = Mesh(structured=True, structured_coords=[cx, cy, cz])
+
+# create a data tag for ww_vals (vector)
+m.tag('ww_n', tagtype=NativeMeshTag, size=len(w_loc), dtype=float)
+
+# create array of values for each mesh voxel, 20 voxels, 5 vals per voxel
+ww_data = np.zeros((num_mesh, num_egrp))
+for grp, loc in w_loc.items():
+    for i in range(len(loc)-1):
+        start = loc[i]
+        end = loc[i+1]
+        for index in range(start, end):
+            ww_data[index][grp] = ww_vals[i]
+
+# populate data
+m.ww_n[:] = ww_data
+
+# save mesh
+m.write_hdf5('ww-mesh.h5m', write_mats=False)
