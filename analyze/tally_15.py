493c603cd1292af773e6ba299755c494ceaf523c
Kalin Kiesling <krkiesling@gmail.com>
2021-07-23 14:06:17 -0500

    switched x axis to wwig ratio and only use upper constant =5

--- analyze/tally_15.py
+++ analyze/tally_15.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import math as m
 
 
-
-
 def calc_sigmas(means, errors, num):
     sigma_upper = means + (means * errors)*num
     sigma_lower = means - (means * errors)*num
@@ -27,87 +25,93 @@ def calc_ratios(m1, e1, m2, e2):
 
 ## Data
 
-# upper bound ratios
-c_u = [20, 50, 100]
+ratios = np.array([5, 20, 50, 100])
+wwig_res = np.array([6.82e-8, 6.8638E-08, 6.81e-8, 6.8e-8])  # placeholders
+wwig_err = np.array([.01, 0.0140, .015, .018])  # placeholders
+wwinp_res = np.array([6.8328E-08, 6.8328E-08, 6.8328E-08, 6.8328E-08])
+wwinp_err = np.array([0.0076, 0.0076, 0.0076, 0.0076])
+analog_res = np.array([6.47480E-08, 6.47480E-08, 6.47480E-08, 6.47480E-08])
+analog_err = np.array([0.0475, 0.0475, 0.0475, 0.0475])
 
-wwig20 = np.array([6.92970E-08, 6.92190E-08, 6.90550E-08])
-wwig20_err = np.array([0.0197, 0.024, 0.0286])
-
-wwig50 = np.array([6.74430E-08, 7.00660E-08, 7.01590E-08])
-wwig50_err = np.array([0.0244, 0.0352, 0.0362])
-
-wwig100 = np.array([6.77020E-08, 6.96540E-08, 6.94170E-08])
-wwig100_err = np.array([0.0264, 0.0297, 0.0301])
-
-wwinp = np.array([6.88180E-08, 7.07220E-08, 6.87680E-08])
-wwinp_err = np.array([0.0128, 0.0198, 0.022])
-
-analog = np.array([6.47480E-08, 6.47480E-08, 6.47480E-08])
-analog_err = np.array([0.0475, 0.0475, 0.0475])
-
-
-wwinp1_upper, wwinp1_lower = calc_sigmas(wwinp, wwinp_err, 1)
-wwinp2_upper, wwinp2_lower = calc_sigmas(wwinp, wwinp_err, 2)
+wwinp1_upper, wwinp1_lower = calc_sigmas(wwinp_res, wwinp_err, 1)
+wwinp2_upper, wwinp2_lower = calc_sigmas(wwinp_res, wwinp_err, 2)
 
 
 # Plots
 
-wwinp_color = '#6FDE6E'
-wwig_color = ['#0A284B', '#235FA4', '#A691AE']
-analog_color = '#E8F086'
+wwinp_color = '#FF4242'
+wwig_color = '#235FA4'
+analog_color = '#6FDE6E'
 
-extra_color = '#FF4242'
+# all colors: '#FF4242', '#E8F086', '#6FDE6E', '#0A284B', '#235FA4', '#A691AE'
 
 cs = 5  # cap size
 dpi = 300
 
-# raw results compared to 1, 2, sigma of wwinp value
+# plot results
 plt.figure()
-plt.plot(c_u, wwinp, label='WWINP', ls='', marker='o', lw=.75, color=wwinp_color)
-plt.plot(c_u, wwinp1_upper, label='WWINP +/- 1 sigma', ls='--', lw=.75, color=wwinp_color)
-plt.plot(c_u, wwinp1_lower, label='', ls='--', lw=.75, color=wwinp_color)
-plt.plot(c_u, wwinp2_upper, label='WWINP +/- 2 sigma', ls=':', lw=.75, color=wwinp_color)
-plt.plot(c_u, wwinp2_lower, label='', ls=':', lw=.75, color=wwinp_color)
-plt.errorbar(c_u, analog, yerr=(analog_err*analog), color=analog_color, label='Analog', ls='', marker='D', lw=.9, capsize=cs)
-plt.errorbar(c_u, wwig20, yerr=(wwig20_err*wwig20), color=wwig_color[0], label='WWIG r=20', ls='', marker='d', lw=.9, capsize=cs)
-plt.errorbar(c_u, wwig50, yerr=(wwig50_err*wwig50), color=wwig_color[1], label='WWIG r=50', ls='', marker='d', lw=.9, capsize=cs)
-plt.errorbar(c_u, wwig100, yerr=(wwig100_err*wwig100), color=wwig_color[2], label='WWIG r=100', ls='', marker='d', lw=.9, capsize=cs)
-plt.xlabel('WW Upper Bound Constant c_u')
+plt.errorbar(ratios, wwig_res, yerr=(wwig_err*wwig_res),
+    color=wwig_color, label='WWIG', ls='', marker='d', lw=.9, capsize=cs)
+plt.errorbar([105], analog_res[0], yerr=(analog_err*analog_res)[0],
+    color=analog_color, label='Analog', ls='', marker='d', lw=.9, capsize=cs)
+plt.plot(ratios, wwinp_res, label='WWINP', ls='-', marker='', lw=.9, color=wwinp_color)
+plt.plot(ratios, wwinp1_upper, label='WWINP +/- 1 sigma', ls='--', lw=.75, color=wwinp_color)
+plt.plot(ratios, wwinp1_lower, label='', ls='--', lw=.75, color=wwinp_color)
+plt.plot(ratios, wwinp2_upper, label='WWINP +/- 2 sigma', ls=':', lw=.75, color=wwinp_color)
+plt.plot(ratios, wwinp2_lower, label='', ls=':', lw=.75, color=wwinp_color)
+plt.xlabel('WWIG surface spacing ratio')
 plt.ylabel('Tally')
 plt.title('Point Detector Tally Results')
-plt.legend(loc='best', ncol=2, fontsize='xx-small')
+plt.legend(loc='best', ncol=2, fontsize='x-small')
 plt.savefig('tally15_results.png', dpi=dpi)
 
+# plot wwinp ratio
+r1 = np.array([1, 1, 1, 1])
 
-# plot ratios compared to wwinp
-m20, e20 = calc_ratios(wwig20, wwig20_err, wwinp, wwinp_err)
-m50, e50 = calc_ratios(wwig50, wwig50_err, wwinp, wwinp_err)
-m100, e100 = calc_ratios(wwig100, wwig100_err, wwinp, wwinp_err)
-mana, eana = calc_ratios(analog, analog_err, wwinp, wwinp_err)
+# ratios compared to wwinp results
+wratio, werr = calc_ratios(wwig_res, wwig_err, wwinp_res, wwinp_err)
+aratio, aerr = calc_ratios(analog_res, analog_err, wwinp_res, wwinp_err)
+
+wr1_upper, wr1_lower = calc_sigmas(r1, werr, 1)
+wr2_upper, wr2_lower = calc_sigmas(r1, werr, 2)
+ar1_upper, ar1_lower = calc_sigmas(r1, aerr, 1)
+ar2_upper, ar2_lower = calc_sigmas(r1, aerr, 2)
 
 plt.figure()
-plt.plot(c_u, [1, 1, 1], ls='-', color=wwinp_color, lw=.9)
-plt.errorbar(c_u, m20, yerr=e20, label='WWIG r=20', ls='', marker='d', lw=.9, capsize=cs, color=wwig_color[0])
-plt.errorbar(c_u, m50, yerr=e50, label='WWIG r=50', ls='', marker='d', lw=.9, capsize=cs, color=wwig_color[1])
-plt.errorbar(c_u, m100, yerr=e100, label='WWIG r=100', ls='', marker='d', lw=.9, capsize=cs, color=wwig_color[2])
-plt.errorbar(c_u, mana, yerr=eana, label='WWIG analog', ls='', marker='D', lw=.9, capsize=cs, color=analog_color)
-plt.xlabel('WW Upper Bound Constant c_u')
+plt.plot(ratios, r1, label='E/WWINP=1', ls='-', marker='', lw=.9, color=wwinp_color)
+
+plt.plot(ratios, wr1_upper, label='WWIG/WWINP +/- 1 sigma', ls='--', lw=.75, color=wwig_color)
+plt.plot(ratios, wr1_lower, label='', ls='--', lw=.75, color=wwig_color)
+plt.plot(ratios, wr2_upper, label='WWIG/WWINP +/- 2 sigma', ls=':', lw=.75, color=wwig_color)
+plt.plot(ratios, wr2_lower, label='', ls=':', lw=.75, color=wwig_color)
+plt.plot(ratios, wratio, label='WWIG/WWINP', ls='', marker='d', lw=.9, color=wwig_color)
+
+# figure out how I want to display the analog/wwinp ratio on this plot
+#plt.plot(ratios, ar1_upper, label='Analog/WWINP +/- 1 sigma', ls='--', lw=.75, color=analog_color)
+#plt.plot(ratios, ar1_lower, label='', ls='--', lw=.75, color=analog_color)
+#plt.plot(ratios, ar2_upper, label='Analog/WWINP +/- 2 sigma', ls=':', lw=.75, color=analog_color)
+#plt.plot(ratios, ar2_lower, label='', ls=':', lw=.75, color=analog_color)
+#plt.plot(ratios, aratio, label='Analog/WWINP', ls='', marker='d', lw=.9, color=analog_color)
+# make this plot a subplot right next to it?
+plt.errorbar([105], [1], yerr=(aratio*aerr)[0], label='analog/WWINP +/- 1 sigma', ls='', marker='', lw=.9, capsize=cs, color=analog_color)
+plt.plot([105], aratio[0], label='analog/WWINP', ls='', marker='d', lw=.9, color=analog_color)
+
+plt.xlabel('WWIG surface spacing ratio')
 plt.ylabel('Ratio (E/WWINP)')
 plt.title('Point Detector Tally Results, compared to WWINP results')
-plt.legend(loc='best', ncol=4, fontsize='xx-small', title='Comparison Results E')
+plt.legend(loc='best', ncol=2, fontsize='x-small', title='Comparison Results E')
 plt.savefig('tally15_ratios.png', dpi=dpi)
+
 
 # plot relative errors
 plt.figure()
-plt.plot(c_u, analog_err, label='analog', ls='-', marker='', lw=.9, color=analog_color)
-plt.plot(c_u, wwinp_err, label='WWINP', ls='-', marker='o', lw=.9, color=wwinp_color)
-plt.plot(c_u, wwig20_err, label='WWIG r=20', ls='-', marker='d', lw=.9, color=wwig_color[0])
-plt.plot(c_u, wwig50_err, label='WWIG r=50', ls='-', marker='d', lw=.9, color=wwig_color[1])
-plt.plot(c_u, wwig100_err, label='WWIG r=100', ls='-', marker='d', lw=.9, color=wwig_color[2])
-plt.xlabel('WW Upper Bound Constant c_u')
+plt.plot(ratios, wwinp_err, label='WWINP', ls='-', marker='', lw=.9, color=wwinp_color)
+plt.plot(ratios, wwig_err, label='WWIG', ls='-', marker='d', lw=.9, color=wwig_color)
+plt.plot(ratios, analog_err, label='analog', ls='-', marker='', lw=.9, color=analog_color)
+plt.xlabel('WWIG surface spacing ratio')
 plt.ylabel('Relative Error')
 plt.title('Point Detector Tally Relative Error')
-plt.legend(loc='best', ncol=3, fontsize='xx-small')
+plt.legend(loc='best', fontsize='x-small')
 plt.savefig('tally15_error.png', dpi=dpi)
 
 plt.show()
