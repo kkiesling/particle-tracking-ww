6f9c118b04c3a1feb365b48f53c863f66b6df008
Kalin Kiesling <krkiesling@gmail.com>
2021-09-09 15:46:06 -0500

    add new data, rework plots to use a reference solution instead of wwinp

--- analyze/tally_15.py
+++ analyze/tally_15.py
@@ -24,26 +24,31 @@ def calc_ratios(m1, e1, m2, e2):
 
 
 ## Data
+ratios = np.array([5, 10, 15, 20])
+wwinp_res = np.full(len(ratios), 5.7436E-08)
+wwinp_err = np.full(len(ratios), 0.0202)
+wwig_res = np.array([5.7301E-08, 5.7690E-08, 5.7053E-08, 5.9092E-08])
+wwig_err = np.array([0.0282, 0.0398, 0.0434, 0.0416])
+analog_res = np.full(len(ratios), 5.5893E-08)
+analog_err = np.full(len(ratios), 0.0646)
 
-ratios = np.array([20, 50, 100])
-wwig_res = np.array([6.8638E-08, 6.8536E-08, 6.9097E-08])
-wwig_err = np.array([0.0140, 0.0178, 0.0204])
-wwinp_res = np.array([6.8328E-08, 6.8328E-08, 6.8328E-08])
-wwinp_err = np.array([0.0076, 0.0076, 0.0076])
-analog_res = np.array([6.47480E-08, 6.47480E-08, 6.47480E-08])
-analog_err = np.array([0.0475, 0.0475, 0.0475])
+# reference solutions
+ref_res = np.full(len(ratios) + 1, 5.5894E-08)
+ref_err = np.full(len(ratios) + 1, 0.0076)
 
-wwinp1_upper, wwinp1_lower = calc_sigmas(wwinp_res, wwinp_err, 1)
-wwinp2_upper, wwinp2_lower = calc_sigmas(wwinp_res, wwinp_err, 2)
+ref1_upper, ref1_lower = calc_sigmas(ref_res, ref_err, 1)
+ref2_upper, ref2_lower = calc_sigmas(ref_res, ref_err, 2)
 
+## Plots
+# stuff for plots
+ext_x = max(ratios) + 1
+ref_xs = np.append(ratios, ext_x)
 
-# Plots
-
-wwinp_color = '#FF4242'
+# all colors: '#FF4242', '#235FA4', '#6FDE6E', '#E8F086', '#0A284B', '#A691AE'
+wwinp_color = '#A691AE'
 wwig_color = '#235FA4'
-analog_color = '#6FDE6E'
-
-# all colors: '#FF4242', '#E8F086', '#6FDE6E', '#0A284B', '#235FA4', '#A691AE'
+analog_color = '#FF4242'
+ref_color = '#6FDE6E'
 
 cs = 5  # cap size
 dpi = 300
@@ -51,15 +56,17 @@ dpi = 300
 # plot results
 plt.figure()
 plt.errorbar(ratios, wwig_res, yerr=(wwig_err*wwig_res),
-    color=wwig_color, label='WWIG', ls='', marker='d', lw=.9, capsize=cs)
-plt.errorbar([105], analog_res[0], yerr=(analog_err*analog_res)[0],
-    color=analog_color, label='Analog', ls='', marker='d', lw=.9, capsize=cs)
-plt.plot(ratios, wwinp_res, label='WWINP', ls='-', marker='', lw=.9, color=wwinp_color)
-plt.plot(ratios, wwinp1_upper, label='WWINP $\pm 1\sigma$', ls='--', lw=.75, color=wwinp_color)
-plt.plot(ratios, wwinp1_lower, label='', ls='--', lw=.75, color=wwinp_color)
-plt.plot(ratios, wwinp2_upper, label='WWINP $\pm 2\sigma$',
-         ls=':', lw=.75, color=wwinp_color)
-plt.plot(ratios, wwinp2_lower, label='', ls=':', lw=.75, color=wwinp_color)
+             color=wwig_color, label='WWIG', ls='', marker='d', lw=.9, capsize=cs)
+plt.errorbar([ext_x], wwinp_res[0], yerr=(wwinp_err*wwinp_res)[0],
+             color=wwinp_color, label='WWINP', ls='', marker='d', lw=.9, capsize=cs)
+plt.errorbar([ext_x], analog_res[0], yerr=(analog_err*analog_res)[0],
+             color=analog_color, label='Analog', ls='', marker='d', lw=.9, capsize=cs)
+plt.plot(ref_xs, ref_res, label='Reference', ls='-', marker='', lw=.9, color=ref_color)
+plt.plot(ref_xs, ref1_upper, label='Reference $\pm 1\sigma$', ls='--', lw=.75, color=ref_color)
+plt.plot(ref_xs, ref1_lower, label='', ls='--', lw=.75, color=ref_color)
+plt.plot(ref_xs, ref2_upper, label='Reference $\pm 2\sigma$',
+         ls=':', lw=.75, color=ref_color)
+plt.plot(ref_xs, ref2_lower, label='', ls=':', lw=.75, color=ref_color)
 plt.xlabel('WWIG surface spacing ratio')
 plt.ylabel('Tally')
 plt.title('Point Detector Tally Results')
@@ -67,48 +74,56 @@ plt.legend(loc='best', ncol=2, fontsize='x-small')
 plt.savefig('tally15_results.png', dpi=dpi)
 
 # plot wwinp ratio
-r1 = np.array([1, 1, 1])
+r1 = np.full(len(ratios), 1)
 
-# ratios compared to wwinp results
-wratio, werr = calc_ratios(wwig_res, wwig_err, wwinp_res, wwinp_err)
-aratio, aerr = calc_ratios(analog_res, analog_err, wwinp_res, wwinp_err)
+# results compared to reference results (ratios)
+wgratio, wgerr = calc_ratios(wwig_res, wwig_err, ref_res[:len(ratios)], ref_err[:len(ratios)])
+wpratio, wperr = calc_ratios(
+    wwinp_res[0], wwinp_err[0], ref_res[0], ref_err[0])
+aratio, aerr = calc_ratios(
+    analog_res[0], analog_err[0], ref_res[0], ref_err[0])
 
-wr1_upper, wr1_lower = calc_sigmas(r1, werr, 1)
-wr2_upper, wr2_lower = calc_sigmas(r1, werr, 2)
+wgr1_upper, wgr1_lower = calc_sigmas(r1, wgerr, 1)
+wgr2_upper, wgr2_lower = calc_sigmas(r1, wgerr, 2)
+
+wpr1_upper, wpr1_lower = calc_sigmas(r1, wperr, 1)
+wpr2_upper, wpr2_lower = calc_sigmas(r1, wperr, 2)
+
 ar1_upper, ar1_lower = calc_sigmas(r1, aerr, 1)
 ar2_upper, ar2_lower = calc_sigmas(r1, aerr, 2)
 
 plt.figure()
-plt.plot(ratios, r1, label='E/WWINP=1', ls='-', marker='', lw=.9, color=wwinp_color)
+# reference
+plt.plot(ratios, r1, label='E/Ref=1', ls='-', marker='', lw=.9, color=ref_color)
 
-plt.plot(ratios, wr1_upper, label='WWIG/WWINP $\pm 1\sigma$',
+# wwig/reference
+plt.plot(ratios, wgratio, label='WWIG/Ref', ls='', marker='d', lw=.9, color=wwig_color)
+plt.plot(ratios, wgr1_upper, label='WWIG/Ref $\pm 1\sigma$',
          ls='--', lw=.75, color=wwig_color)
-plt.plot(ratios, wr1_lower, label='', ls='--', lw=.75, color=wwig_color)
-plt.plot(ratios, wr2_upper, label='WWIG/WWINP $\pm 2\sigma$',
+plt.plot(ratios, wgr1_lower, label='', ls='--', lw=.75, color=wwig_color)
+plt.plot(ratios, wgr2_upper, label='WWIG/Ref $\pm 2\sigma$',
          ls=':', lw=.75, color=wwig_color)
-plt.plot(ratios, wr2_lower, label='', ls=':', lw=.75, color=wwig_color)
-plt.plot(ratios, wratio, label='WWIG/WWINP', ls='', marker='d', lw=.9, color=wwig_color)
-
-# figure out how I want to display the analog/wwinp ratio on this plot
-#plt.plot(ratios, ar1_upper, label='Analog/WWINP +/- 1 sigma', ls='--', lw=.75, color=analog_color)
-#plt.plot(ratios, ar1_lower, label='', ls='--', lw=.75, color=analog_color)
-#plt.plot(ratios, ar2_upper, label='Analog/WWINP +/- 2 sigma', ls=':', lw=.75, color=analog_color)
-#plt.plot(ratios, ar2_lower, label='', ls=':', lw=.75, color=analog_color)
-#plt.plot(ratios, aratio, label='Analog/WWINP', ls='', marker='d', lw=.9, color=analog_color)
-# make this plot a subplot right next to it?
-plt.errorbar([105], [1], yerr=(aratio*aerr)[0], label='Analog/WWINP $\pm 1\sigma$',
+plt.plot(ratios, wgr2_lower, label='', ls=':', lw=.75, color=wwig_color)
+# wwinp/reference
+plt.errorbar(ext_x, [1], yerr=(wpratio*wperr), label='WWINP/Ref $\pm 1\sigma$',
+             ls='', marker='', lw=.9, capsize=cs, color=wwinp_color)
+plt.plot(ext_x, wpratio, label='WWINP/Ref', ls='',
+         marker='d', lw=.9, color=wwinp_color)
+# analog/reference
+plt.errorbar(ext_x, [1], yerr=(aratio*aerr), label='Analog/Ref $\pm 1\sigma$',
              ls='', marker='', lw=.9, capsize=cs, color=analog_color)
-plt.plot([105], aratio[0], label='Analog/WWINP', ls='', marker='d', lw=.9, color=analog_color)
+plt.plot(ext_x, aratio, label='Analog/Ref', ls='', marker='d', lw=.9, color=analog_color)
 
 plt.xlabel('WWIG surface spacing ratio')
-plt.ylabel('Ratio (E/WWINP)')
-plt.title('Point Detector Tally Results, compared to WWINP results')
-plt.legend(loc='best', ncol=2, fontsize='x-small', title='Comparison Results E')
+plt.ylabel('Ratio (E/Reference)')
+plt.title('Point Detector Tally Results,\n compared to reference results')
+plt.legend(loc='best', ncol=3, fontsize='x-small', title='Comparison Results E')
 plt.savefig('tally15_ratios.png', dpi=dpi)
-
 
 # plot relative errors
 plt.figure()
+plt.plot(ref_xs[:len(ratios)], ref_err[:len(ratios)], label='Reference', ls='-',
+         marker='', lw=.9, color=ref_color)
 plt.plot(ratios, wwinp_err, label='WWINP', ls='-', marker='', lw=.9, color=wwinp_color)
 plt.plot(ratios, wwig_err, label='WWIG', ls='-', marker='d', lw=.9, color=wwig_color)
 plt.plot(ratios, analog_err, label='Analog', ls='-', marker='', lw=.9, color=analog_color)
