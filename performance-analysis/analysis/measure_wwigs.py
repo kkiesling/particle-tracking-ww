7383222cf040976edf5121e80a7d81823ccd0520
Kalin Kiesling <krkiesling@gmail.com>
2021-10-19 15:14:09 -0500

    add file size measurement

--- performance-analysis/analysis/measure_wwigs.py
+++ performance-analysis/analysis/measure_wwigs.py
@@ -2,17 +2,20 @@ import dagmc_stats.DagmcFile as df
 import dagmc_stats.DagmcQuery as dq
 import sys
 import numpy as np
+import os
 
 if __name__ == "__main__":
 
     flist = sys.argv[1:]
 
     for f in flist:
+        fsize = os.path.getsize(f) / (1024.**2)
 
         gfile = df.DagmcFile(f)
         gqu = dq.DagmcQuery(gfile)
         gqu.calc_coarseness()
 
         print(f)
+        print('file size (MB): {}'.format(fsize))
         print(gqu._surf_data)
         print('average: {}'.format(np.average(gqu._surf_data['coarseness'])))
