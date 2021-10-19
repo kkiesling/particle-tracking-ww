23b4f0f6183341240269da0409257283feb130d9
Kalin Kiesling <krkiesling@gmail.com>
2021-10-19 11:22:37 -0500

    playing around with relative error distribution plots

--- performance-analysis/analysis/plot_relerr_dist.py
+++ performance-analysis/analysis/plot_relerr_dist.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 def plot_dens_pdf(bins, data, title, color):
     vals = np.clip(data, bins[0], bins[-1])
-    sns.distplot(vals, kde=True, hist=True, norm_hist=True,
+    sns.distplot(vals, kde=True, hist=False, norm_hist=False,
                  color=color, label=title)
 
 
@@ -27,39 +27,37 @@ def get_data(fh5m):
 def get_name(meshfile):
     """Strings for title and save names."""
 
-    # name = meshfile.split('/')[1].split('-')[0]
-    # if name == 'analog':
-    #     title = 'Analog'
-    # elif name == 'wwinp':
-    #     title = 'Cartesian WW Mesh'
-    # elif name == 'wwig':
-    #     title = 'WWIG'
-
-    name = 'rel_err_plot'
-    title = 'relative error'
+    name = meshfile.split('/')[-1].split('_')[0]
+    if name == 'analog':
+        title = 'Analog'
+    elif name == 'cwwm':
+        title = 'Cartesian WW Mesh'
+    elif name == 'wwig':
+        title = 'WWIG'
 
     return name, title
 
 
 if __name__ == '__main__':
 
-    legends = []
-    all_data = []
+    legends = {}
+    all_data = {}
 
     # files should be the h5m files for the meshtally
 
     for fh5m in sys.argv[1:]:
-        save_name, title = get_name(fh5m)
-        legends.append(title)
-        data = get_data(fh5m)
-        all_data.append(data)
+        name, title = get_name(fh5m)
+        legends[name] = title
+        all_data[name] = get_data(fh5m)
 
-    colors = ['r', 'b', 'g']
+    colors = {'cwwm': '#A691AE',
+              'analog': '#FF4242',
+              'wwig': '#6FDE6E'}
     bins = np.linspace(0.0, 1.0, num=11)
 
     # plot PDFs
-    for i, data in enumerate(all_data):
-        plot_dens_pdf(bins, data, legends[i], colors[i])
+    for name, data in all_data.items():
+        plot_dens_pdf(bins, data, legends[name], colors[name])
 
     plt.xlim([0, bins[-1]])
     plt.xlabel('relative error R')
@@ -71,8 +69,8 @@ if __name__ == '__main__':
     plt.clf()
 
     # plot CDFs
-    for i, data in enumerate(all_data):
-        plot_dens_cdf(bins, data, legends[i], colors[i])
+    for name, data in all_data.items():
+        plot_dens_cdf(bins, data, legends[name], colors[name])
 
     plt.xlim([0, bins[-1]])
     plt.xlabel('relative error R')
