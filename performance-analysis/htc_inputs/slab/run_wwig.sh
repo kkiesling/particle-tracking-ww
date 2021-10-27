ed57356592ac3b754b9f93d8d3283d849632a5a1
Kalin Kiesling <krkiesling@gmail.com>
2021-10-27 12:45:41 -0500

    corrected submit scripts

--- performance-analysis/htc_inputs/slab/run_wwig.sh
+++ performance-analysis/htc_inputs/slab/run_wwig.sh
@@ -6,8 +6,15 @@ source /home/cnerg/.bashrc
 # set datapath
 export DATAPATH=/staging/kkiesling/MCNP_DATA
 
+
+
 # only use the "no viz" ones
+rm -r 000/vols/ 000/levelfile 000/visitlog.py
 mv 000/wwn_000_noviz.h5m 000/wwn_000.h5m
+
+echo `ls -l .`
+echo `ls -l 000/`
+echo `pwd`
 
 mcnp6 i=wwig_slab.inp wcad=000/
 
