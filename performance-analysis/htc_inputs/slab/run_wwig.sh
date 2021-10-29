ec70a0159444b42d67bd6657f539fb8d405369c9
Kalin Kiesling <krkiesling@gmail.com>
2021-10-29 09:23:04 -0500

    updated run scripts passing a single geom file instead of whole folder

--- performance-analysis/htc_inputs/slab/run_wwig.sh
+++ performance-analysis/htc_inputs/slab/run_wwig.sh
@@ -6,15 +6,8 @@ source /home/cnerg/.bashrc
 # set datapath
 export DATAPATH=/staging/kkiesling/MCNP_DATA
 
-
-
 # only use the "no viz" ones
-rm -r 000/vols/ 000/levelfile 000/visitlog.py
-mv 000/wwn_000_noviz.h5m 000/wwn_000.h5m
+mkdir geoms
+mv wwn_000_noviz.h5m geoms/wwn_000.h5m
 
-echo `ls -l .`
-echo `ls -l 000/`
-echo `pwd`
-
-mcnp6 i=wwig_slab.inp wcad=000/
-
+mcnp6 i=wwig_slab.inp wcad=geoms/
