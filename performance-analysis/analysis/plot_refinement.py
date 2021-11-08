694104b8cf81cff99e71c8bc6d5529ec312caa4b
Kalin Kiesling <krkiesling@gmail.com>
2021-11-08 12:06:03 -0600

    updated colors, plot file size for ratio

--- performance-analysis/analysis/plot_refinement.py
+++ performance-analysis/analysis/plot_refinement.py
@@ -1,22 +1,27 @@
-import sys
+import os
 import numpy as np
 from matplotlib import legend
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 
-colors = {0: '#FF4242', 1: '#235FA4', 2: '#6FDE6E', 'cwwm': '#E8F086',
-          'analog': '#0A284B', 'reference': '#A691AE', 3: '#0A284B'}
-color_r = {5: '#FF4242', 6: '#235FA4', 7: '#6FDE6E', 8: '#E8F086',
-           9: '#0A284B', 10: '#A691AE'}
-markers = {0: 'x', 1: 'x', 2: 'x', 'cwwm': 'o',
-           'analog': 'd', 'reference': 'D'}
+wwig_colors = ['#F3B4CB', '#EA84AA', '#FF5A96', '#FF0A64', '#C11D59',
+               '#86284A', '#653345', '#67072A', '#3C0346', '#60226B',
+               '#831596', '#814A8A', '#B106CE', '#BE58D0', '#DF9EEA',
+               '#F6C1FF']
+colors = {'wwig': '#C11D59', 'analog': '#7CC145',
+          'cwwm': '#247F03', 'reference': '#24E2A5'}
+markers = {'wwig': 'd', 'cwwm': 'o',
+           'analog': 'x', 'reference': 'X'}
 e_bounds = {0: '1.0000E-05', 1: '1.0000E-01', 2: '2.0000E+01', 3: 'total'}
 ratios = [5, 6, 7, 8, 9, 10]
 dpi = 600
 lw = .9  # line width for plots
 cs = 5  # error bar cap size
 n_sigma = 3
+
+fcwwm = '../inputs/wwinp_slab'
+cwwm_size = os.path.getsize(fcwwm) / (1024.**2)
 
 
 def plot_fsize(df):
@@ -28,15 +33,43 @@ def plot_fsize(df):
 
     plt.plot(df.loc[df['group'] == 'total']['dc factor'],
              df.loc[df['group'] == 'total']['size'],
-             linestyle='', marker='d', color=colors[0])
+             linestyle='', marker='d', color=colors['wwig'], label='WWIG')
+    plt.plot([min(df['dc factor']), max(df['dc factor'])],
+             [cwwm_size, cwwm_size],
+             linestyle=':', marker='', color=colors['cwwm'], label='CWWM')
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.legend(loc='best', ncol=2,
+               fontsize='x-small')
     plt.tight_layout()
 
     save_name = 'images/fsize_decimated.png'
     plt.savefig(save_name)
-    # eventually need to add line for WWINP file size!!
+
+
+def plot_fsize_ratio(df):
+    # plot total file size - decimated sizes only
+    plt.figure()
+    title = 'File Size'
+    xlabel = 'WWIG Surface Spacing Ratio'
+    ylabel = 'Total File Size (MB)'
+
+    plt.plot(df.loc[df['group'] == 'total']['ratio'],
+             df.loc[df['group'] == 'total']['size'],
+             linestyle='', marker='d', color=colors['wwig'], label='WWIG')
+    plt.plot([min(df['ratio']), max(df['ratio'])],
+             [cwwm_size, cwwm_size],
+             linestyle=':', marker='', color=colors['cwwm'], label='CWWM')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(loc='best', ncol=2,
+               fontsize='x-small')
+    plt.tight_layout()
+
+    save_name = 'images/fsize_ratio.png'
+    plt.savefig(save_name)
 
 
 def plot_average_coarseness(df):
@@ -48,7 +81,7 @@ def plot_average_coarseness(df):
 
     plt.plot(df.loc[df['group'] == 'total']['dc factor'],
              df.loc[df['group'] == 'total']['average coarseness'],
-             linestyle='', marker='d', color=colors[0])
+             linestyle='', marker='d', color=colors['wwig'])
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -70,7 +103,7 @@ def subplot_roughness(df, group_min, group_max):
         df_sub = df.loc[df['group'] == str(group)]
         ax[pr][pc].plot(df_sub['perturbation'],
                         df_sub['average roughness'],
-                        linestyle='', marker='d', color=colors[0])
+                        linestyle='', marker='d', color=colors['wwig'])
         title_str = 'E_{}'.format(group)
         ax[pr][pc].set_title('$E_{' + str(group) + '}$')
 
@@ -93,7 +126,7 @@ def plot_average_roughness(df):
 
     plt.plot(df.loc[df['group'] == 'total']['perturbation'],
              df.loc[df['group'] == 'total']['average roughness'],
-             linestyle='', marker='d', color=colors[0])
+             linestyle='', marker='d', color=colors['wwig'])
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -120,5 +153,10 @@ if __name__ == '__main__':
     print(df_rough.keys())
     plot_roughness_per_group(df_rough)
     plot_average_roughness(df_rough)
+
+    fratio = 'csv/wwig_ratio_size.csv'
+    df_ratio = pd.read_csv(fratio, header=0, index_col=0)
+    print(df_ratio.keys())
+    plot_fsize_ratio(df_ratio)
 
     plt.show()
